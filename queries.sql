6fc591f347d96aee5e59dd75d0a10b964ce89730
Cadde0 <ocaddeo@kth.se>
2024-11-26 15:43:17 +0100

    Query 2 & 3

--- queries.sql
+++ queries.sql
@@ -21,5 +21,60 @@ GROUP BY
 ORDER BY 
     EXTRACT(MONTH FROM activity.start_time);
 
+/* Query 2 */
 
-    
+-- Count the number of siblings for each student
+WITH sibling_count AS ( 
+SELECT 
+    student_school_id_first AS student_id,
+    COUNT(student_school_id_second) AS no_of_siblings
+FROM sibling
+GROUP BY student_school_id_first
+UNION ALL
+SELECT 
+    student_school_id_second AS student_id,
+    COUNT(student_school_id_first) AS no_of_siblings
+FROM sibling
+GROUP BY student_school_id_second
+),
+-- Aggregate sibling count for each student
+total_sibling_count AS (
+SELECT 
+    student_id,
+    SUM(no_of_siblings) AS no_of_siblings_sum
+FROM sibling_count
+GROUP BY student_id
+)
+-- Count students by their sibling count
+SELECT
+    COALESCE(no_of_siblings_sum,0) AS "No of Siblings",
+    COUNT(*) AS "No of Students"
+FROM student
+LEFT JOIN
+    total_sibling_count ON student.student_school_id = total_sibling_count.student_id
+GROUP BY
+    "No of Siblings"
+ORDER BY "No of Siblings";
+
+
+/* Query 3 */
+
+WITH lessons_this_month AS (
+    SELECT 
+        instructor_school_id, 
+        COUNT(*) AS total_lessons
+    FROM activity
+    WHERE 
+        EXTRACT(MONTH FROM start_time) = EXTRACT(MONTH FROM CURRENT_DATE)
+        AND EXTRACT(YEAR FROM start_time) = EXTRACT(YEAR FROM CURRENT_DATE)
+    GROUP BY instructor_school_id
+)
+SELECT 
+    person.school_id AS "Instructor Id", 
+    person.first_name AS "First Name",
+    person.last_name AS "Last Name", 
+    total_lessons AS "No of Lessons"
+FROM lessons_this_month
+JOIN person ON lessons_this_month.instructor_school_id = person.school_id
+WHERE lessons_this_month.total_lessons > 0 -- number of given lessons
+ORDER BY "No of Lessons" DESC;
