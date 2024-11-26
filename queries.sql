ca4f5f37923eacee27a23b15cf9f6be43d8bcdb6
JonathanVarild <jonathan.varild@gmail.com>
2024-11-26 17:14:01 +0100

    Added query to get slots left at ensembles

--- queries.sql
+++ queries.sql
@@ -21,6 +21,7 @@ GROUP BY
 ORDER BY 
     EXTRACT(MONTH FROM activity.start_time);
 
+
 /* Query 2 */
 
 -- Count the number of siblings for each student
@@ -78,3 +79,34 @@ FROM lessons_this_month
 JOIN person ON lessons_this_month.instructor_school_id = person.school_id
 WHERE lessons_this_month.total_lessons > 0 -- number of given lessons
 ORDER BY "No of Lessons" DESC;
+
+
+/* Query 4 */
+
+WITH ensambles_next_week AS (
+  SELECT activity.activity_id,
+    ensamble.genre,
+    ensamble.max_students,
+    activity.start_time
+  from activity,
+    ensamble
+  WHERE start_time >= DATE_TRUNC('week', CURRENT_DATE) + INTERVAL '7 days'
+    AND start_time < DATE_TRUNC('week', CURRENT_DATE) + INTERVAL '14 days'
+    AND activity.activity_id = ensamble.activity_id
+),
+bookings_per_ensamble AS (
+  SELECT booking.activity_id,
+    COUNT(booking.student_school_id) as booked_students
+  FROM ensambles_next_week
+    LEFT JOIN booking ON ensambles_next_week.activity_id = booking.activity_id
+  GROUP BY booking.activity_id
+)
+SELECT TO_CHAR(start_time, 'Dy') as "Day",
+  genre as "Genre",
+  CASE
+    WHEN max_students - COALESCE(booked_students, 0) <= 0 THEN 'No seats'
+    WHEN max_students - COALESCE(booked_students, 0) <= 2 THEN '1 or 2 seats'
+    ELSE 'Many seats'
+  END AS "No of Free Seats"
+FROM ensambles_next_week
+  LEFT JOIN bookings_per_ensamble on ensambles_next_week.activity_id = bookings_per_ensamble.activity_id
